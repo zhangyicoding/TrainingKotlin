9bbfd09b5e4248cbc513b99667b301b3ecc001e8
estyle <1115144299@qq.com>
2018-06-19 00:06:41 +0800

    kt笔记

--- app/src/main/java/com/estyle/trainingkotlin/fragments/No03Fragment.kt
+++ app/src/main/java/com/estyle/trainingkotlin/fragments/No03Fragment.kt
@@ -63,8 +63,9 @@ class No03Fragment : BaseFragment() {
     }
 
     private fun showTitleByEveryWord() {
-        val title = """为啥要学Kotlin开发Android——为了替换Java。
-                        |Java说它不服，接下来我们让它死得明白""".trimMargin()
+//        val title = """为啥要学Kotlin开发Android——为了替换Java。
+//                        |Java说它不服，接下来我们让它死得明白""".trimMargin()
+        val title = "为啥要学Kotlin开发Android——为了替换Java。"
 
         val sb = StringBuilder()
         rightOne01TV.visibility = View.VISIBLE
